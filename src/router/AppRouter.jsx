14c558474e0342a92d45edc79c2fe97ee5c851d8
Nishant Paliwal <paliwalnishant0@gmail.com>
2025-10-17 11:55:47 +0530

    Merge my stashed changes with friend’s updates

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,6 +4,8 @@ import SignInPage from '../pages/SignInPage';
 import SignUpPage from '../pages/SignUpPage';
 import DashboardPage from '../pages/DashboardPage';
 import CalenderPage from '../pages/CalenderPage';
+import EmployeeFormPage from '../pages/EmployeeFormPage';
+import EmployeeDetailsPage from '../pages/EmployeeDetailsPage';
 
 const AppRouter = () => {
   return (
@@ -14,6 +16,8 @@ const AppRouter = () => {
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/calendar" element={<CalenderPage />} />
+        <Route path="/add-employee" element={<EmployeeFormPage />} />
+        <Route path="/employee_details" element={<EmployeeDetailsPage/>} />
         
       </Routes>
     </Router>
