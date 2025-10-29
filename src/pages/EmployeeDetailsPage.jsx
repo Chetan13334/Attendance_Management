e0bf3f5ab2ec5ee6c6849e5557fe3efc916b39c5
Nishant Paliwal <paliwalnishant0@gmail.com>
2025-10-29 14:52:36 +0530

    improve the ui and add functionality

--- src/pages/EmployeeDetailsPage.jsx
+++ src/pages/EmployeeDetailsPage.jsx
@@ -35,13 +35,13 @@ const EmployeeDetailsPage = () => {
     navigate("/signin");
   };
 
-  if (!user) {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <div className="text-gray-600">Loading Dashboard...</div>
-      </div>
-    );
-  }
+  // if (!user) {
+  //   return (
+  //     <div className="min-h-screen flex items-center justify-center bg-gray-100">
+  //       <div className="text-gray-600">Loading Dashboard...</div>
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className="min-h-screen bg-gray-100">
