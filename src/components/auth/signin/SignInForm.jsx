af344d92c7bebd581d26febd3f690744675a1310
Nishant Paliwal <paliwalnishant0@gmail.com>
2025-11-12 11:25:17 +0530

    fix the sign in form gile

--- src/components/auth/signin/SignInForm.jsx
+++ src/components/auth/signin/SignInForm.jsx
@@ -2,42 +2,18 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { signInWithEmail, signInWithGoogle, setShowSuccessMessage } from "../../../redux/slices/authSlice";
+import SignInContainer from "./SignIn.container";
 
-const SuccessToast = () => /* @__PURE__ */ jsxDEV(
-  "div",
-  {
-    className: "fixed top-4 left-1/2 -translate-x-1/2 z-50 p-4 w-11/12 max-w-sm rounded-xl bg-green-50 text-green-800 shadow-2xl border border-green-200 transition-all duration-500 ease-in-out transform scale-100 opacity-100",
-    role: "alert"
-  },
-  /* @__PURE__ */ jsxDEV(
-    "div",
-    {
-      className: "flex items-center"
-    },
-    void 0,
-    false,
-    {
-      fileName: "src/components/auth/signin/SignInForm.jsx",
-      lineNumber: 10,
-      columnNumber: 5
-    },
-    this
-  ),
-  false,
-  {
-    fileName: "src/components/auth/signin/SignInForm.jsx",
-    lineNumber: 7,
-    columnNumber: 3
-  },
-  this
+const SuccessToast = () => (
+  <div className="fixed top-4 left-1/2 -translate-x-1/2 z-50 p-4 w-11/12 max-w-sm rounded-xl bg-green-50 text-green-800 shadow-2xl border border-green-200 transition-all duration-500 ease-in-out transform scale-100 opacity-100">
+    <div className="flex items-center">
+      {/* Content would go here */}
+    </div>
+  </div>
 );
 
 const SignInForm = () => {
-  return /* @__PURE__ */ jsxDEV(SignInContainer, {}, void 0, false, {
-    fileName: "src/components/auth/signin/SignInForm.jsx",
-    lineNumber: 50,
-    columnNumber: 10
-  }, this);
+  return <SignInContainer />;
 };
 
 export default SignInForm;
